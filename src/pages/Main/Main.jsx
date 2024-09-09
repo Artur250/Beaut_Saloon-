76be6ceee89464467c6167ab48c31b00a170f465
User <user@MacBook-Air.local>
2024-09-09 17:40:06 +0600

    styles and adding images and modal windows

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
-import "./Main.css";
+import './Main.css';
 import Modal from '../Modal/Modal'; 
+import Sugar_depi from '../../assets/Sugar-img.jpeg';
+import Woxe_depi from '../../assets/woxe-img.jpeg'
+import Electro_depi from '../../assets/Elektro.webp'
 
 const Main = () => {
     const [modalSugarActive, setModalSugarActive] = useState(false);
     const [modalWaxActive, setModalWaxActive] = useState(false);
     const [modalElectroActive, setModalElectroActive] = useState(false);
+
     return (
         <div className='container'>
-           
-            <main>
-                <button className='open-btn' onClick={() => setModalSugarActive(true)}>Открыть модальное окно 1</button>
-                <button className='open-btn' onClick={() => setModalWaxActive(true)}>Открыть модальное окно 2</button>
-                <button className='open-btn' onClick={() => setModalElectroActive(true)}>Открыть модальное окно 3</button>
+            <main className='modal-main'>
+                <div className='Sugar-dep'>
+                    <img className='img_dep' src={Sugar_depi} alt="Sugar depilation" />
+                    <button className='open-btn' onClick={() => setModalSugarActive(true)}>Sugar depilation</button>
+                </div>
+                <div  className='Wax-dep'>
+                    <img className='img_dep' src={Woxe_depi} alt="Wax depilation" />
+                    <button className='open-btn' onClick={() => setModalWaxActive(true)}>Wax depilation</button>
+                </div>
+                <div className='Electro-dep'>
+                    <img className='img_dep' src={Electro_depi} alt="Electrolysis" />
+                    <button className='open-btn' onClick={() => setModalElectroActive(true)}>Electrolysis</button>
+                </div>
             </main>
             <Modal active={modalSugarActive} setActive={setModalSugarActive}>
                 <p>1</p>
